de2d94c319064a1b1f16b49eedf98b4e35eac92f
Damilare <opeyemisamuel222@gmail.com>
2021-03-08 16:41:08 +0000

    extracting key logic

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,7 @@
-class Sorter {
-  constructor(public collection: number[]) {}
-
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-
-          this.collection[j] = this.collection[j + 1];
-
-          this.collection[j + 1] = leftHand;
-        }
-      }
-    }
-  }
-}
 
 
-const sorter = new Sorter([-3, -1, 8, 2])
+const sorter = new Sorter([-3, -1, 8, 2]);
 
-sorter.sort()
+sorter.sort();
 
-console.log(sorter.collection)
+console.log(sorter.collection);
