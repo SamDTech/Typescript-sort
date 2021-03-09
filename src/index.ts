0dd2d32858120223c839e98202d71fc2f107294c
Damilare <opeyemisamuel222@gmail.com>
2021-03-09 14:14:02 +0000

    call linked list in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 
@@ -9,10 +10,21 @@ import { Sorter } from './Sorter';
 
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('string');
+// const charactersCollection = new CharactersCollection('string');
 
-const sorter = new Sorter(charactersCollection);
+// const sorter = new Sorter(charactersCollection);
 
-sorter.sort();
+// sorter.sort();
 
-console.log(charactersCollection);
+// console.log(charactersCollection);
+
+const linkedList = new LinkedList()
+
+linkedList.add(-50)
+linkedList.add(4)
+linkedList.add(43)
+linkedList.add(8)
+
+const sorter = new Sorter(linkedList)
+
+sorter.sort()
